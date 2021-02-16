2deb87053c29453d4e56a8c1b51b7b48022e8124
marcussu <marcussu@marcussu.com>
2021-02-16 15:55:34 +0800

    Update Web3 lib to v1.3.4. Bug fixes.

--- js/web3_ashare.js
+++ js/web3_ashare.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {
     window.web3 = new Web3(window.ethereum);
     window.ethereum.request({ method: 'eth_requestAccounts' });
 	// "currently selected" address is the first item in the array returned by eth_accounts
-    const account = window.ethereum.request({ method: 'eth_accounts' });
+    const accounts = window.ethereum.request({ method: 'eth_accounts' });
 	account = accounts[0];
     console.log("connected with: "+account);
 	
